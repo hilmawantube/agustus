7b28dd17cedb71bb7eae8a37d62cd248d9000809
Hilmawan2 <61814317+Hilmawan2@users.noreply.github.com>
2020-07-23 05:01:37 +0700

    Update gopay.php

--- gopay.php
+++ gopay.php
@@ -5,9 +5,9 @@ ulang:
 // function change(){
 echo "\e[93m<=========🅷 🅸 🅻 🅼 🅰 🆆 🅰 🅽 🆃 🆄 🅱 🅴=========> \n";
 echo "\e[32m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n";
-echo "\e[32m         SCRIFT GOJEK 21 JULI       \n";
+echo "\e[32m         SCRIFT GOJEK 23 JULI       \n";
 echo "\e[93m  ===================================== \n";
-echo "\e[93m PASTIKAN SUDAH MENGGUNAKAN FAKE GPS     \n";
+echo "\e[93m   PASTIKAN SUDAH MENGGUNAKAN FAKE GPS     \n";
 echo "\e[96m        GUNAKAN UNTUK MAKAN         \n";
 echo "\e[93m  ======================================\n";
 echo "\e[93m       \e[32m AUTO CLAIM VOCHER 20K \e[93m \n";
